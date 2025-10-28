c04afebe7791832c4a9f6ca4091207a37adc4574
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2025-10-28 20:23:57 +0100

    add hair removal func

--- src/preproc_utils/preprocessing.py
+++ src/preproc_utils/preprocessing.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import numpy as np
 from PIL import Image, ImageFilter
+import cv2
 
 def load_image(path: Path) -> Image.Image:
     return Image.open(path).convert("RGB")
@@ -9,8 +10,24 @@ def load_mask(path: Path) -> Image.Image:
     return Image.open(path).convert("L")
 
 def remove_hairs(img: Image.Image) -> Image.Image:
-    # Placeholder do usuwania włosów; na razie filtr medianowy
-    return img.filter(ImageFilter.MedianFilter(size=3))
+    # na podstawie publikacji PMC10969337
+    # Konwersja PIL do OpenCV (numpy)
+    img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
+
+    # utworzenie maski wlosow za pomoca blackhat
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+    blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, kernel)
+
+    # binaryzacja maski wlosow
+    _, hair_mask = cv2.threshold(blackhat, 10, 255, cv2.THRESH_BINARY)
+
+    # inpainting do usuniecia wlosow
+    inpainted = cv2.inpaint(img_cv, hair_mask, inpaintRadius=1, flags=cv2.INPAINT_TELEA)
+
+    # konwersja do pil
+    inpainted_rgb = cv2.cvtColor(inpainted, cv2.COLOR_BGR2RGB)
+    return Image.fromarray(inpainted_rgb)
 
 def resize_image(img: Image.Image, size: tuple[int, int], is_mask: bool = False) -> Image.Image:
     resample = Image.NEAREST if is_mask else Image.BILINEAR
