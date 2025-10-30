19009cb0f87c980b0b89832df705bf8a5053f765
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2025-10-30 17:21:07 +0100

    add maau augmentations

---
+++ src/data/augmentations.py
@@ -0,0 +1,21 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+def get_augmentations_maau():
+    # augemntacje na podstawie publikacji PMID: 37189563
+    return A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=20, p=0.5),
+        A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2()
+    ])
+
+def get_augmentations_noisy_student():
+    # TODO: dodac augmentacje
+    pass
+
+def get_augmentations_dumm():
+    # TODO: dodac augmentacje
+    pass
